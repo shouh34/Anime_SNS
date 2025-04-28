4106dad60037c6ce9ef878797a72cf736c93e1d1
shouh34 <shouheirai@gmail.com>
2025-04-29 07:21:20 +0900

    プッシュしました

--- resources/views/Dashbord.blade.php
+++ resources/views/Dashbord.blade.php
@@ -1,5 +1,7 @@
 <html>
 <head>
+    <link rel="icon" href="{{ asset('images/favicon.ico') }}" type="image/x-icon">
+
   <!-- Bootstrap CSS -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
@@ -23,43 +25,76 @@ font-size: 30px;
 
 
 }
-
+.dropdown-menu {
+    transform: translateY(10px);
+    opacity: 0;
+    transition: all 0.3s ease;
+}
+.dropdown-menu.show {
+    transform: translateY(0);
+    opacity: 1;
+}
 </style>
 </head>
-<body><!-- ナビゲーションバー -->
+<body>
+
+
     <div class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarAccordion" aria-controls="navbarAccordion" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto"> <!-- ms-auto を追加して右寄せ -->
-                    <form method="POST" action="{{ route('anime.search') }}" class="row g-3 mb-4">
-                        <div class="col-md-8">
-                            <input type="text" name="t1" size="50" class="form-control"  placeholder="検索したい内容を入力してください">
-                        </div>
-                        <div class="col-md-4">
-                            <button type="submit" class="btn btn-primary w-100">検索</button>
-                        </div>
-                    </form>
+    
+            <div class="collapse navbar-collapse" id="navbarAccordion">
+                <ul class="navbar-nav ms-auto"> <!-- ms-autoで右寄せ -->
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('profile') }}">プロフィール</a>
+                        <form method="POST" action="{{ route('anime.search') }}" class="row g-3 mb-4">
+                            @csrf
+                            <div class="col-md-8">
+                                <input type="text" name="t1" size="50" class="form-control" placeholder="検索したい内容を入力してください">
+                            </div>
+                            <div class="col-md-4">
+                                <button type="submit" class="btn btn-primary w-100">検索</button>
+                            </div>
+                        </form>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('anime.search') }}">アニメ検索</a>
+    
+                    <!-- ここが検索のドロップダウン -->
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            検索
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="searchDropdown">
+                            <li><a class="dropdown-item" href=>ユーザー検索</a></li>
+                            <li><a class="dropdown-item" href="{{ route('anime.search') }}">アニメ検索</a></li>
+                        </ul>
                     </li>
+    
                     <li class="nav-item">
                         <a class="nav-link" href="{{ route('anime.seasonal') }}">季節アニメ</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="{{ route('DM') }}">DM</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('ImgBBS') }}">スレッド作成</a>
+
+
+
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="threadDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            スレッド
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="threadDropdown">
+                            <li><a class="dropdown-item" href="{{ route('ImgBBS') }}">スレッド作成</a></li>
+                            <li><a class="dropdown-item" href="{{ route('ImgBBS_view') }}">スレッド一覧</a></li>
+                        </ul>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('ImgBBS_view') }}">スレッド一覧</a>
-                    </li>
+                    
+
+
+
+                </li>
+
                     <li class="nav-item">
                         <a class="nav-link" href="{{ route('Settings') }}">設定</a>
                     </li>
@@ -72,7 +107,10 @@ font-size: 30px;
     </div>
     
 
-<!-- ダッシュボードコンテンツ -->
+
+
+
+    <!-- ダッシュボードコンテンツ -->
 <div class="container-fluid mt-4">
 
     <!-- ダッシュボード内容 -->
